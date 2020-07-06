3c055c0cd9466681566635f20b3d5652a31ad65f
mkatrodia <45700458+mkatrodia@users.noreply.github.com>
2020-07-06 11:43:06 -0400

    adding first card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,24 @@ import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { CardComponent } from './card/card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidenavComponent
+    SidenavComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,9 @@ import { SidenavComponent } from './sidenav/sidenav.component';
     AppRoutingModule,
     MatSliderModule,
     MatToolbarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule
 
   ],
   providers: [],
