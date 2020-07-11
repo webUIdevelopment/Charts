b3c2a6947434825ce3e178c9f8ff5b78cc5ba69b
Dharmesh P <ipatel.ui@gmail.com>
2020-07-11 09:22:37 -0700

    remvoing logic from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,5 @@ export class AppComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // give everything a chance to get loaded before starting the animation to reduce choppiness
-    setTimeout(() => {
-      this.generateData();
-
-      // change the data periodically
-      setInterval(() => this.generateData(), 5000);
-    }, 1000);
-  }
-
-  generateData() {
-    this.chartData = [];
-    for (let i = 0; i < 8 + Math.floor(Math.random() * 10); i++) {
-      this.chartData.push([`Index ${i}`, Math.floor(Math.random() * 100)]);
-    }
   }
 }
